c5f10b533f60ac07478a67c57114d143785048a8
yonahasi0911 <shota-y-17d@stu.kbc.ac.jp>
2019-10-09 09:26:49 +0900

    生成処理追加(作業途中なので動きません)

---
+++ Assets/Yonaha_shtoa/Script/Shota_Obj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shota_Obj : MonoBehaviour {
+    [SerializeField] float fFallSpeed;// 落下速度
+    [SerializeField] float fMaxFallSpeed;// 最大落下速度
+    float fTotalFallTime; // 落下時間
+    [SerializeField]　float DeathPosition; // これ以下になると消える
+
+    private void Update()
+    {
+        if(fFallSpeed  * fTotalFallTime < fMaxFallSpeed)
+        fTotalFallTime += Time.deltaTime;
+
+        transform.Translate(0, -fFallSpeed * fTotalFallTime, 0);
+
+        if(transform.position.y <= -DeathPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Yonaha_shtoa/Script/Shota_ObjState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Shota_ObjState : ScriptableObject {
+    
+    [SerializeField] int iReminingNumber; // 残り数
+    [SerializeField] float fGenerateRate; // 出現確率
+    [SerializeField] GameObject Obj; // 生成対象
+
+    public int GetiReminingNumber()
+    {
+        return iReminingNumber;
+    }
+
+    public float GetGenerateRate()
+    {
+        return fGenerateRate;
+    }
+
+    public GameObject GetObject()
+    {
+        return Obj;
+    }
+
+    public void AddGenerateRate(float addNum)
+    {
+        fGenerateRate += addNum;
+    }
+
+    public void SubtractionReminingNumber(int subNum)
+    {
+        iReminingNumber -= subNum;
+    }
+
+}
