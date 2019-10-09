9b4737a457720f542884a4cfeb9cb8ef8107cfc1
yonahasi0911 <shota-y-17d@stu.kbc.ac.jp>
2019-10-09 17:52:56 +0900

    原因とみられるシーンファイルのみ除去しました

--- Assets/Yonaha_shtoa/Script/Shota_ObjState.cs
+++ Assets/Yonaha_shtoa/Script/Shota_ObjState.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Shota_ObjState : ScriptableObject {
     
-    [SerializeField] int iReminingNumber; // 残り数
-    [SerializeField] float fGenerateRate; // 出現確率
+    [SerializeField] int iReminingNumber; // 残り数設定用変数
+    [SerializeField] float fGenerateRate; // 出現確率設定用変数
     [SerializeField] GameObject Obj; // 生成対象
+
+
+    [SerializeField] int CurrentReminingNumber; // 実際に変動する値
+    [SerializeField] float CurrentGenerateRate; // 実際に変動する値
+
+
+    private void OnEnable()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        CurrentReminingNumber = iReminingNumber;
+        CurrentGenerateRate = fGenerateRate;
+    }
 
     public int GetiReminingNumber()
     {
-        return iReminingNumber;
+        return CurrentReminingNumber;
     }
 
     public float GetGenerateRate()
     {
-        return fGenerateRate;
+        return CurrentGenerateRate;
     }
 
     public GameObject GetObject()
@@ -26,12 +42,11 @@ public class Shota_ObjState : ScriptableObject {
 
     public void AddGenerateRate(float addNum)
     {
-        fGenerateRate += addNum;
+        CurrentGenerateRate += addNum;
     }
 
     public void SubtractionReminingNumber(int subNum)
     {
-        iReminingNumber -= subNum;
+        CurrentReminingNumber -= subNum;
     }
-
 }
