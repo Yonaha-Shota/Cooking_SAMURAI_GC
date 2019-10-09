2c7642d70df90fb6f79dfb998a878de0b431d09a
tatsuki929 <11807113@stu.kbc.ac.jp>
2019-10-09 18:19:10 +0900

    .unityファイル追加、並びにスクリプトに解説追加。

--- Assets/ituki/MoveChara.cs
+++ Assets/ituki/MoveChara.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class MoveChara : MonoBehaviour {
-    //定数
+
+   //定数
     private const float LEFT = -7.0f;
     private const float RIGHT = 3.0f;
 
     //変数
     public const float Y = -3.75f;
     public float speed = -2.5f;
+
     public float X = -7.0f;
+
+    // Use this for initialization
+    void Start () {
+      
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if ((X > LEFT -1 ) && (X < RIGHT + 1 )) { 
+        if ((X > LEFT - 1) && (X < RIGHT + 1)) { 
             if(Input.GetKeyDown(KeyCode.LeftArrow)||Input.GetKeyDown(KeyCode.RightArrow))//左右キーいずれかを押したときのみ
              X += (Input.GetAxisRaw("Horizontal") * speed);//左右を判断しXを±1する
         }
@@ -23,9 +29,9 @@ public class MoveChara : MonoBehaviour {
         if (X <= LEFT){X = LEFT;}//範囲指定
         if (X >= RIGHT){X = RIGHT;}//範囲指定
 
-        //Vector2 direction = new Vector2(X, 0).normalized; //不要になりました。
+        //不要　Vector2 direction = new Vector2(X, 0).normalized;
 
-        // 位置移動移動
+        // 位置移動
         GetComponent<Rigidbody2D>().position = new Vector2(X,0);
 
 
