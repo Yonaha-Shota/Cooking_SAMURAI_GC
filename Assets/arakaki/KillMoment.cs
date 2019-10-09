e91cd75a91d25f43c38bd5915ffe49e476c89977
Tepodon <hiromu13411234@gmail.com>
2019-10-09 09:49:36 +0900

    切る処理の一部(未完成)と野菜の落ちる処理(仮)の追加

---
+++ Assets/arakaki/KillMoment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMoment : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("----斬----");
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           
+            Destroy(other.gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+             //Destroy(gameObject);
+        
+    }
+}
